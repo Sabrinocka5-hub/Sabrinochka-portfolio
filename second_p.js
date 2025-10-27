373f0480d1c2921a0757d3da400d0a8792afb645
Sabrinocka5-hub <anel.kozhanova5@gmail.com>
2025-10-27 19:49:57 +0500

    Update second_p.js

--- second_p.js
+++ second_p.js
@@ -1,11 +1,15 @@
 function openProject(num) {
   const projectPaths = {
-    1: "Project1/index.html",
-    3: "Project3/index.html",
-    4: "Project4/index.html",
-    5: "Project5/index.html"
+    1: "Project1",
+    3: "Project3",
+    4: "Project4",
+    5: "Project5"
   };
 
-  const encodedPath = encodeURI(projectPaths[num]);
-  window.open(encodedPath, "_blank");
+  // правильный путь для GitHub Pages
+  const baseURL = "https://sabrinocka5-hub.github.io/Sabrinochka-portfolio/";
+
+  // открываем index.html внутри каждой папки проекта
+  const projectURL = `${baseURL}${projectPaths[num]}/index.html`;
+  window.open(projectURL, "_blank");
 }
